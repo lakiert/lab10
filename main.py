e614d0c7cc72850f196781f50a36ab4a7375ce83
lakiert <aleksandra.lakiert@gmail.com>
2021-05-06 14:26:10 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# zad1
+# x = np.arange(0, 21, 1)
+# y = (1/x)
+# plt.plot(x, y, label='f(x)')
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.ylim(0, 1)
+# plt.xlim(1, len(x))
+# plt.legend()
+# plt.show()
+
+# zad2
+x = np.arange(0, 21, 1)
+y = (1/x)
+plt.plot(x, y, label='f(x)')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.ylim(0, 1)
+plt.xlim(0, len(x))
+plt.legend()
+plt.show()
